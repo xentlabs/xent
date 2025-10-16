73699db536aa288aad16b5677cfd4e65236372d7
Andrew Emil <ace@xentlabs.ai>
2025-10-16 15:53:56 -0400

    Add full support for haltable game play

---
+++ src/xent/benchmark/run_haltable.py
@@ -0,0 +1,78 @@
+import logging
+from typing import Any
+
+from xent.common.configuration_types import ExecutableGameMap
+from xent.runtime.execution import Results, State, run_haltable_game
+from xent.runtime.judge import Judge
+from xent.runtime.players import make_npcs, make_player
+from xent.runtime.runtime import XentRuntime
+from xent.runtime.variables import build_globals, build_locals
+
+
+async def start_haltable_game(
+    executable_game_map: ExecutableGameMap,
+    judge: Judge | None,
+) -> Results | State:
+    game_name = executable_game_map["game_map"]["name"]
+    map_seed = executable_game_map["game_map"]["map_seed"]
+    player_id = executable_game_map["player"]["id"]
+    game_code = executable_game_map["game_map"]["code"]
+    game_str = f"{game_name} ({map_seed}) for player: {player_id}"
+
+    if judge is None:
+        judge = Judge(executable_game_map["metadata"]["judge_model"])
+        judge.set_seed(executable_game_map["metadata"]["seed"], "")
+
+    player = make_player(executable_game_map)
+    npcs = make_npcs(executable_game_map)
+    locals = build_locals(player, npcs, executable_game_map)
+    globals = build_globals(judge)
+
+    xrt = XentRuntime(
+        player,
+        npcs,
+        locals,
+        globals,
+        store_full_interactions=executable_game_map["metadata"].get(
+            "store_full_player_interactions", False
+        ),
+    )
+    lines = [line.strip() for line in game_code.split("\n")]
+
+    logging.info(f"Running game: {game_str}")
+    game_results = await run_haltable_game(
+        lines, xrt, executable_game_map["metadata"]["num_rounds_per_game"], 0, []
+    )
+    if game_results["kind"] == "results":
+        logging.info("Haltable game completed")
+        return game_results
+    else:
+        serialized_game_state = {
+            "game_state": game_results["state"],
+            "judge": judge.serialize(),
+        }
+        return {"kind": "state", "state": serialized_game_state}
+
+
+async def resume_haltable_game(state: dict[str, Any]) -> Results | State:
+    judge = Judge.deserialize(state["judge"])
+    globals = build_globals(judge)
+    xrt = XentRuntime.deserialize(state["game_state"]["runtime"], globals)
+    lines: list[str] = state["game_state"]["code"]
+    rounds_played = state["game_state"]["rounds_played"]
+    round_results = state["game_state"]["round_results"]
+    num_rounds = state["num_rounds"]
+
+    logging.info("Resuming game")
+    game_results = await run_haltable_game(
+        lines, xrt, num_rounds, rounds_played, round_results
+    )
+    if game_results["kind"] == "results":
+        logging.info("Haltable game completed")
+        return game_results
+    else:
+        serialized_game_state = {
+            "game_state": game_results["state"],
+            "judge": judge.serialize(),
+        }
+        return {"kind": "state", "state": serialized_game_state}
--- src/xent/runtime/execution.py
+++ src/xent/runtime/execution.py
@@ -1,12 +1,13 @@
 import ast
 import logging
-from typing import Any
+from typing import Any, Literal, TypedDict
 
 from xent.common.configuration_types import GameMapRoundResult
 from xent.common.errors import (
     XentConfigurationError,
     XentError,
     XentGameError,
+    XentHaltMessage,
     XentInternalError,
     XentSyntaxError,
 )
@@ -82,6 +83,72 @@ async def play_game(
 
     logging.info("Game completed successfully")
     return round_results
+
+
+class Results(TypedDict):
+    kind: Literal["results"]
+    results: list["GameMapRoundResult"]
+
+
+class State(TypedDict):
+    kind: Literal["state"]
+    state: dict[str, Any]
+
+
+async def start_haltable_game(
+    lines: list[str],
+    xrt: XentRuntime,
+    num_rounds: int,
+    rounds_played: int,
+    round_results: list[GameMapRoundResult],
+) -> Results | State:
+    while rounds_played < num_rounds:
+        try:
+            result = await play_single_game(lines, xrt, rounds_played)
+            if result is None:
+                return {"kind": "results", "results": []}  # TODO what to do here?
+            rounds_played += 1
+            round_results.append(result)
+        except XentHaltMessage:
+            serialized_game_state: dict[str, Any] = {
+                "lines": lines,
+                "num_rounds": num_rounds,
+                "runtime": xrt.serialize(),
+                "rounds_played": rounds_played,
+                "round_results": round_results,
+            }
+            return {"kind": "state", "state": serialized_game_state}
+
+    logging.info("Game completed successfully")
+    return {"kind": "results", "results": round_results}
+
+
+async def resume_haltable_game(state: dict[str, Any]) -> Results | State:
+    code: str = state["code"]
+    rounds_played = state["rounds_played"]
+    round_results = state["round_results"]
+    code = state["code"]
+    num_rounds = state["num_rounds"]
+    xrt = XentRuntime.deserialize(state["runtime"])
+    while rounds_played < num_rounds:
+        try:
+            result = await play_single_game(lines, xrt, rounds_played)
+            if result is None:
+                return {"kind": "results", "results": []}  # TODO what to do here?
+            rounds_played += 1
+            round_results.append(result)
+        except XentHaltMessage:
+            serialized_game_state: dict[str, Any] = {
+                "code": code,
+                "num_rounds": num_rounds,
+                "runtime": xrt.serialize(),
+                "rounds_played": rounds_played,
+                "round_results": round_results,
+            }
+            return {"kind": "state", "state": serialized_game_state}
+
+    logging.info("Game completed successfully")
+    return {"kind": "results", "results": round_results}
 
 
 # TODO need to clean up error handling / none return here
